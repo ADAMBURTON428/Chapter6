95384cdd526f2024d1e0d22a2b4c642cca6ddb24
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-10-01 09:53:09 -0500

    update

--- Chapter6/src/excercises/TestScoreStatistics.java
+++ Chapter6/src/excercises/TestScoreStatistics.java
@@ -2,8 +2,9 @@ package excercises;
 
 public class TestScoreStatistics {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
